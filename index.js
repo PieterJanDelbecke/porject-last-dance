14a834eca7bc7662cc2578bdb01b908ed9d4e6b6
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-27 08:26:03 +1100

    initialise Objects

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const initialRows = [
-    { name: "one", info: { order: 1, color: "yellow" } },
-    { name: "two", info: { order: 2, color: "green" } },
-    { name: "three", info: { order: 3, color: "red" } },
-    { name: "four", info: { order: 4, color: "blue" } },
-];
+// const initialRows = [
+//     { name: "one", info: { order: 1, color: "yellow" } },
+//     { name: "two", info: { order: 2, color: "green" } },
+//     { name: "three", info: { order: 3, color: "red" } },
+//     { name: "four", info: { order: 4, color: "blue" } },
+// ];
 
-const myRows = new Map()
-
-for (let row of initialRows){
-    myRows.set(row.name, row.info)
+const initialRows = {
+    firstName: "Pieter-Jan", lastName: "Delbecke", city: "Sydney", country: "Australia"
 }
 
-console.log(myRows.get("four"))
+myObject1 = Object.values(initialRows)
+myObject2 = Object.keys(initialRows)
+
+console.log(myObject1)
+console.log(myObject2)
