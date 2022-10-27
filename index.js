7e0b42c6577d6ba68720b2eac3030b953d8e7e8a
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-28 09:33:33 +1100

    implementation of the set

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// const initialRows = [
-//     { name: "one", info: { order: 1, color: "yellow" } },
-//     { name: "two", info: { order: 2, color: "green" } },
-//     { name: "three", info: { order: 3, color: "red" } },
-//     { name: "four", info: { order: 4, color: "blue" } },
-// ];
+const mySet = new Set()
 
-const initialRows = {
-    firstName: "Pieter-Jan", lastName: "Delbecke", city: "Sydney", country: "Australia"
-}
+mySet.add(1)
+mySet.add(2)
+mySet.add([3,4,5])
 
-const myResult1 = initialRows.firstName
-const myResult2 = initialRows["firstName"]
-
-console.log(myResult1, "++++", myResult2)
+console.log([...mySet])
