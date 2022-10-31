6194c901aa875354d9faceb9cd4a28302308a18b
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-31 13:56:58 +1100

    refactoring of the Map to Object algorithm

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ const myMap = new Map()
 myMap.set("one", {firstName: "Pieter-Jan", lastName: "Delbecke", city: "Sydney" })
 myMap.set("two", {firstName: "Alessandra", lastName: "Salso", city: "Melbourne" })
 
-const myObj = Object.fromEntries(myMap)
-const values = Object.values(myObj)
-
-console.log(values)
+console.log([...myMap.values()])
