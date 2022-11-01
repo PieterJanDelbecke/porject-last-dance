875db9a982c732e0db0556eb8b048de46dd1c68f
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-01 16:09:13 +1100

    refactoring

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const myMap = new Map()
 myMap.set("one", {firstName: "Pieter-Jan", lastName: "Delbecke", city: "Sydney" })
 myMap.set("two", {firstName: "Alessandra", lastName: "Salso", city: "Melbourne" })
 
-console.log([...myMap.values()])
+// console.log([...myMap.values()])
+
+const myArr = [0,2,4]
+
+console.log(myArr[1])
