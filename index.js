038ed31ce6f212afcffead34d27e560f55939320
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-03 09:35:26 +1100

    add optionsMap  to the fields

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const myArr = [0,1,2,3,4,5]
+const tabs = new Map()
+tabs.set("one", { name: "Main", fields: new Map(), data: new Map()})
 
-const lastItem = myArr.at(-1)
+tabs.get("one").fields.set("fieldOne", { name: "fieldName", isHidden: false, order: 0})
 
-console.log(lastItem)
+myTemp = tabs.get("one").fields.get("fieldOne")
+console.log(myTemp) 
+tabs.get("one").fields.set("fieldOne", {...myTemp, options: new Map()})
+tabs.get("one").fields.get("fieldOne").options.set("id1", {color: "red", name: "new"})
+
+myOptions = tabs.get("one").fields.get("fieldOne").options.get("id1")
+console.log(myOptions)
