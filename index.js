07ab16db6cee107656dc443c2071fa70c02beb77
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-03 08:19:07 +1100

    add item to map

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const myMap = new Map()
 
 myMap.set("one", {firstName: "Pieter-Jan", lastName: "Delbecke", city: "Sydney" })
 myMap.set("two", {firstName: "Alessandra", lastName: "Salso", city: "Melbourne" })
+myMap.set("three", {firstName: "Boris", lastName: "Johnston", city: "London" })
 
-console.log(myMap.get("one"))
+console.log(myMap.get("three"))
