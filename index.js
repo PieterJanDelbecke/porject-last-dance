0f32255700c2bacdd9b1798300d0962b4fb2368c
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-06 08:27:25 +1100

    refactoing

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-function getMiddle(s){
-    const length = s.length
-    const half = Math.floor(length/2)
-    if (length % 2 === 0){
-        return s.slice(half-1, half+1)
-    } else {
-        return s.charAt(half)
-    }
+function getMiddle(s) {
+  const half = Math.floor(s.length / 2);
+  return s.length % 2 === 0 ? s.slice(half - 1, half + 1) : s.charAt(half);
 }
 
-
-console.log("test: ", getMiddle("test"))
-console.log("testing: ", getMiddle("testing"))
+console.log("test: ", getMiddle("test"));
+console.log("testing: ", getMiddle("testing"));
