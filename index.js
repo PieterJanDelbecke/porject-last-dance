2bd37e18f9664b4d8f0280b9708d674b772f3165
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-06 08:23:48 +1100

    slicing of the incoming string

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const tabs = new Map()
-tabs.set("one", { name: "Main", fields: new Map(), data: new Map()})
+function getMiddle(s){
+    const length = s.length
+    const half = Math.floor(length/2)
+    if (length % 2 === 0){
+        return s.slice(half-1, half+1)
+    } else {
+        return s.charAt(half)
+    }
+}
 
-tabs.get("one").fields.set("fieldOne", { name: "fieldName", isHidden: false, order: 0})
 
-myTemp = tabs.get("one").fields.get("fieldOne")
-console.log(myTemp) 
-tabs.get("one").fields.set("fieldOne", {...myTemp, options: new Map()})
-tabs.get("one").fields.get("fieldOne").options.set("id1", {color: "red", name: "new"})
-
-myOptions = tabs.get("one").fields.get("fieldOne").options.get("id1")
-console.log(myOptions)
-console.log(myOptions)
-console.log(myOptions)
-
+console.log("test: ", getMiddle("test"))
+console.log("testing: ", getMiddle("testing"))
