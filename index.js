174d01fd0c62061c744b5942b0c79179d234a909
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-07 15:34:33 +1100

    get min value out of array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function findShort(s){
-     return s.split(" ").map(i => i.length)
+     const arr = s.split(" ").map(i => i.length)
+     return Math.min(...arr)
 }
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
