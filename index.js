f39757310bfc671befd6de6424c5550d06180b75
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-08 08:52:23 +1100

    capitalisation of algorithem updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function accum(s) {
   return s.split("").map((i, index) => {
-     const string = i.repeat(index+1)
+     const string = i.toLowerCase().repeat(index+1)
      return i.repeat(index+1).charAt(0).toUpperCase() + string.substring(1)
   }).join("-");
 }
