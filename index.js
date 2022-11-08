3e8c4c5fa4169bfc2915080ee42134b87dd512e4
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-09 09:29:27 +1100

    refactoring of isogram method

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 function isIsogram(str){
-     str = str.toLowerCase()
-     const str1 = str.split("")
-     const mySet= new Set()
-     for( i of str1){
-          mySet.add(i)
-     }
-     const str2 = [...mySet].join("")
-     return (str === str2)? true : false
+     return new Set(str.toLowerCase()).size === str.length
 }
 
 console.log(isIsogram("Dermatoglyphics"))
