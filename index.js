97f6859794147b08a8467195ee1b179632f904c8
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-10 09:43:13 +1100

    refactoring largest method

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function largest(n, xs) {
   return xs
-    .sort((a, b) => b - a)
-    .slice(0, n)
-    .sort((a, b) => a - b);
+    .sort((a, b) => a - b)
+    .slice(-n)
 }
 
 console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
