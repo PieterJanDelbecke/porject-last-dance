95257629bb5f52ad76c8e9450c47bec7656bb5ef
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-10 09:22:45 +1100

    fizzbuzz

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function isIsogram(str){
-     return new Set(str.toLowerCase()).size === str.length
+function fizzbuzz(n) {
+  const arr = [];
+  for (let i = 1 ; i <= n; i++) {
+    let a = "",
+      b = "";
+    if (i % 3 === 0) {
+      a = "Fizz";
+    }
+    if (i % 5 === 0) {
+      b = "Buzz";
+    }
+    const result = a + b ? a + b : i;
+    arr.push(result);
+  }
+  return arr;
 }
 
-console.log(isIsogram("Dermatoglyphics"))
-console.log(isIsogram("isogram"))
-console.log(isIsogram("aba"))
-console.log(isIsogram("bbb"))
-
+console.log(fizzbuzz(20));
