969522771a15ffa15d9e9e992cc3f350c5a8d919
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-10 09:46:00 +1100

    made adjustment to largest method

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function largest(n, xs) {
-  return xs
-    .sort((a, b) => a - b)
-    .slice(-n)
+  return n ? xs.sort((a, b) => a - b).slice(-n) : [];
 }
 
 console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]));
+console.log(largest(0, [5, 1, 5, 2, 3, 1, 2, 3, 5]));
