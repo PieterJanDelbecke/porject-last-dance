28f0129200c556a800f9f8cb98fcab5401dc7981
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-17 09:28:46 +1100

    refactoring

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const date = new Date();
-console.log(date);
+// const date = new Date();
+// console.log(date);
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
+// let day = date.getDate();
+// let month = date.getMonth() + 1;
+// let year = date.getFullYear();
 
-console.log(`today is ${day}/${month}/${year}`);
+// console.log(`today is ${day}/${month}/${year}`);
 
-let currentDate = new Date().toJSON().slice(0, 10);
-console.log(currentDate);
+// let currentDate = new Date().toJSON().slice(0, 10);
+// console.log(currentDate);
 
-console.log(typeof currentDate);
+// console.log(typeof currentDate);
 
-let myDate = new Date().toLocaleDateString("au-AU");
-console.log(myDate);
-console.log(myDate);
+// let myDate = new Date().toLocaleDateString("au-AU");
+// console.log(myDate);
+// console.log(myDate);
+
+const myObj = {
+  name: "Pieter-Jan",
+  city: "Sydney",
+};
+
+const myKeys = Object.keys(myObj);
+const myValues = Object.values(myObj);
+
+console.log("Keys:", myKeys);
+console.log("Objects:", myValues);
