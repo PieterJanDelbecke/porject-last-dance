d8b6949c2a188684146cc5891815741ecd81a4bf
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-22 09:10:08 +1100

    review of styling

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function isLeapYear(year) {
-  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
-}
 
-console.log(isLeapYear(1234));
-console.log(isLeapYear(1984));
-console.log(isLeapYear(2000));
-console.log(isLeapYear(2010));
-console.log(isLeapYear(2013));
-console.log(isLeapYear(1900));
