3b1e4b390344785bd2b9f81191159fe54cb717f9
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-12 08:59:06 +1100

    defenders vs attachers results

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function hasSurvive(attackers, defenders) {
   const initPowerAttacker = attackers.reduce((a, b) => a + b);
   const initPowerDefenders = defenders.reduce((a, b) => a + b);
-  return [initPowerAttacker, initPowerDefenders];
+  let attSurvivers = 0;
+  let defSurvivers = 0;
+  for (let i = 0; i < defenders.length; i++) {
+    defenders[i] > attackers[i] ? defSurvivers++ : attSurvivers++;
+  }
+  return [attSurvivers, defSurvivers];
 }
 
 console.log(hasSurvive([2, 9, 9, 7], [1, 1, 3, 8]));
