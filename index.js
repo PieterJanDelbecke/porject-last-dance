e940164a8125167d0b4866922c4913d99f327ac6
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-16 13:25:22 +1100

    sets

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ const myMap = new Map();
 
 myMap.set(1, { name: "Pieter-Jan" });
 
-console.log(myMap);
-console.log(myMap.get(1));
+// console.log(myMap);
+// console.log(myMap.get(1));
+
+const mySet = new Set();
+
+mySet.add(1);
+mySet.add(1);
+mySet.add(1);
+mySet.add(2);
+mySet.add(1);
+mySet.add(1);
+
+console.log(mySet);
