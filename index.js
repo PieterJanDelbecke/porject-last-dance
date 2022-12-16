1193d84f34e157ecd0c8498d04ddb7ca657a81e8
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-12-16 13:16:51 +1100

    blank page

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function hasSurvive(attackers, defenders) {
-  const initPowerAttacker = attackers.reduce((a, b) => a + b);
-  const initPowerDefenders = defenders.reduce((a, b) => a + b);
-  let attSurvivers = 0;
-  let defSurvivers = 0;
-  for (let i = 0; i < defenders.length; i++) {
-    defenders[i] > attackers[i] ? defSurvivers++ : attSurvivers++;
-  }
-  return [attSurvivers, defSurvivers];
-}
 
-console.log(hasSurvive([2, 9, 9, 7], [1, 1, 3, 8]));
