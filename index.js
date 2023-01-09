af0e55889aa145386218fd256439c2170697214d
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-01-10 08:34:36 +1100

    min max

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function disemvowel(str) {
-  const regex = /[aouie]/gi;
-  return str.replace(regex, "");
+function minMinMax(array) {
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+  return [min, max];
 }
 
-console.log(disemvowel("This website is for losers LOL!"));
-console.log(
-  disemvowel("No offense but,\nYour writing is among the worst I've ever read")
-);
-
-console.log("Happy new Year");
+console.log(minMinMax([-1, 4, 5, -23, 24]));
+console.log(minMinMax([1, 3, -3, -2, 8, -1]));
