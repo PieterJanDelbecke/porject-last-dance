87276f27d3d68bf91c98cac8dbee0ae78564377c
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-01-12 09:29:00 +1100

    sum arrays

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function list_filter(l) {
-  return l.filter((a) => typeof a === "number");
+function addingShifted(arrayOfArrays, shift) {
+  const arr1 = arrayOfArrays[0];
+  const arr2 = arrayOfArrays[1];
+  const sumArr = arr1.map((number, index) => {
+    return number + arr2[index];
+  });
+  return sumArr;
 }
 
-console.log(list_filter([1, 2, "a", "b"]));
-console.log(list_filter([1, "a", "b", 0, 15]));
+console.log(
+  addingShifted(
+    [
+      [1, 2, 3, 4, 5, 6],
+      [7, 7, 7, 7, 7, -7],
+    ],
+    0
+  )
+);
