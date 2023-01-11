a7d69e05725a9a1866885d66f2cf3fae75460475
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-01-12 09:44:53 +1100

    add arrays with shift

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function addingShifted(arrayOfArrays, shift) {
   const arr1 = arrayOfArrays[0];
   const arr2 = arrayOfArrays[1];
   const sumArr = arr1.map((number, index) => {
-    return number + arr2[index];
+    if (index < shift) {
+      return number;
+    } else {
+      return number + arr2[index];
+    }
   });
   return sumArr;
 }
@@ -11,8 +15,8 @@ console.log(
   addingShifted(
     [
       [1, 2, 3, 4, 5, 6],
-      [7, 7, 7, 7, 7, -7],
+      [7, 7, 7, 7, 7, 7],
     ],
-    0
+    3
   )
 );
