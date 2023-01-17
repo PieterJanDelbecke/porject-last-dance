03f4377326b95ce44425e96c932351a7f965bcd3
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-01-18 09:27:12 +1100

    getMiddle

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function squareDigits(num) {
-  return +num
-    .toString()
-    .split("")
-    .map((item) => Math.pow(Number(item), 2))
-    .join("");
+function getMiddle(s) {
+  const length = s.length;
+  if (length % 2 === 0) {
+    return s.slice(length / 2 - 1, length / 2 + 1);
+  } else {
+    return s.charAt(Math.floor(length / 2));
+  }
 }
 
-console.log(squareDigits(3212));
+console.log(getMiddle("test"));
+console.log(getMiddle("testing"));
+console.log(getMiddle("middle"));
+console.log(getMiddle("A"));
+
+// console.log(Math.floor(5 / 3));
