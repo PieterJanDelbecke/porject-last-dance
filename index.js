e8235eaf89f801db170f4e14abe00b702c1f1f3b
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-01-23 08:54:19 +1100

    fieldOptions

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const dataFieldOptions = [
-  { fieldId: "CCC", name: "D", sortOrder: 3, default: true },
-  { fieldId: "AAA", name: "yesterday", sortOrder: 2, default: false },
-  { fieldId: "BBB", name: "four", sortOrder: 3, default: true },
-  { fieldId: "AAA", name: "old", sortOrder: 1, default: false },
-  { fieldId: "CCC", name: "A", sortOrder: 0, default: true },
-  { fieldId: "BBB", name: "three", sortOrder: 2, default: false },
-  { fieldId: "CCC", name: "B", sortOrder: 1, default: false },
-  { fieldId: "CCC", name: "C", sortOrder: 2, default: false },
-  { fieldId: "AAA", name: "tomorrow", sortOrder: 3, default: true },
-  { fieldId: "BBB", name: "two", sortOrder: 1, default: false },
-  { fieldId: "BBB", name: "one", sortOrder: 0, default: true },
-  { fieldId: "CCC", name: "E", sortOrder: 4, default: true },
-  { fieldId: "AAA", name: "new", sortOrder: 0, default: true },
-];
+// const dataFieldOptions = [
+//   { fieldId: "CCC", name: "D", sortOrder: 3, default: true },
+//   { fieldId: "AAA", name: "yesterday", sortOrder: 2, default: false },
+//   { fieldId: "BBB", name: "four", sortOrder: 3, default: true },
+//   { fieldId: "AAA", name: "old", sortOrder: 1, default: false },
+//   { fieldId: "CCC", name: "A", sortOrder: 0, default: true },
+//   { fieldId: "BBB", name: "three", sortOrder: 2, default: false },
+//   { fieldId: "CCC", name: "B", sortOrder: 1, default: false },
+//   { fieldId: "CCC", name: "C", sortOrder: 2, default: false },
+//   { fieldId: "AAA", name: "tomorrow", sortOrder: 3, default: true },
+//   { fieldId: "BBB", name: "two", sortOrder: 1, default: false },
+//   { fieldId: "BBB", name: "one", sortOrder: 0, default: true },
+//   { fieldId: "CCC", name: "E", sortOrder: 4, default: true },
+//   { fieldId: "AAA", name: "new", sortOrder: 0, default: true },
+// ];
 
-// console.log(
-//   "dataFieldOption sorted",
-//   dataFieldOptions.sort((a, b) => a.sortOrder - b.sortOrder)
-// );
+// // console.log(
+// //   "dataFieldOption sorted",
+// //   dataFieldOptions.sort((a, b) => a.sortOrder - b.sortOrder)
+// // );
+
+// const fieldOptions = {};
+
+// for (const option of dataFieldOptions.sort(
+//   (a, b) => a.sortOrder - b.sortOrder
+// )) {
+//   if (!fieldOptions[option.fieldId]) {
+//     fieldOptions[option.fieldId] = [];
+//   }
+//   fieldOptions[option.fieldId].push(option);
+// }
+
+// console.log("fieldOptions", fieldOptions);
 
 const fieldOptions = {};
 
-for (const option of dataFieldOptions.sort(
-  (a, b) => a.sortOrder - b.sortOrder
-)) {
-  if (!fieldOptions[option.fieldId]) {
-    fieldOptions[option.fieldId] = [];
-  }
-  fieldOptions[option.fieldId].push(option);
-}
+fieldOptions["Hello"] = [];
+fieldOptions["World"] = [];
+fieldOptions["Hello"].push({ x: 5, y: 10 });
 
-console.log("fieldOptions", fieldOptions);
+console.log(fieldOptions);
