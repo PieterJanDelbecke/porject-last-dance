3a109ea2f8e5b7a57fc6dd4d98b3c0900f38a8a4
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-03-02 09:20:59 +1100

    review2

--- index2.js
+++ index2.js
@@ -1,5 +1,10 @@
 const arr = [1, 2, 3];
 
-let total = 0;
-arr.forEach((e) => (total = total + e));
-console.log(total);
+// let total1 = 0;
+// arr.forEach((e) => (total1 = total1 + e));
+// console.log(total1);
+let arr1 = arr.forEach((e) => e + 2);
+let arr2 = arr.map((e) => e + 2);
+
+console.log(arr1);
+console.log(arr2);
