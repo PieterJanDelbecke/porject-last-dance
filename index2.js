de7452d25aa0c3525f8b7f83d003322d546b53e4
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-03-05 09:57:32 +1100

    increase sortOrder

--- index2.js
+++ index2.js
@@ -1,12 +1,5 @@
-const arr = [1, 2, 3];
+let sortOrder = 0;
 
-// let total1 = 0;
-// arr.forEach((e) => (total1 = total1 + e));
-// console.log(total1);
-let arr1 = arr.forEach((e) => e + 2);
-let arr2 = arr.map((e) => e + 2);
-
-console.log(arr1);
-console.log(arr2);
-
-console.log("hello world");
+for (i = 0; i <= 10; i++) {
+  console.log(++sortOrder);
+}
